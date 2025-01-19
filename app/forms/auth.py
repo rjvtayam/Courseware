fc966f32bf79fd6df863e7b2c9442ef3c357eb7e
rjvtayam <rickyjhun.tayam@lspu.edu.ph>
2025-01-20 01:52:23 +0800

    refactor: Simplify auth forms to match OAuth-only authentication
    - Remove unused LoginForm and RegistrationForm
    - Add TermsAgreementForm for terms and conditions checkbox
    - Clean up imports and validators

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -1,29 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
-from app.models import User
+from wtforms import BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    is_teacher = BooleanField('Register as Teacher')
-    submit = SubmitField('Register')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different username.')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different email address.')
+class TermsAgreementForm(FlaskForm):
+    """Form for agreeing to terms and conditions"""
+    agree_terms = BooleanField('I agree to the Terms of Service and Privacy Policy', 
+                             validators=[DataRequired(message='You must agree to the terms to continue')])
+    submit = SubmitField('Continue')
